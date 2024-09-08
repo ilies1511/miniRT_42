76ac1c6a29078700b02a6b69a15937b763137579
ilies1511 <iziane@student.42heilbronn.de>
2024-09-08 19:20:15 +0200

    REFACTOR(mlx_interface): replace MLX_PRESS with MLX_RELEASE

--- srcs/ft_engine/mlx_interface/hooks.c
+++ srcs/ft_engine/mlx_interface/hooks.c
@@ -13,6 +13,6 @@ void	main_key_hooks(mlx_key_data_t keydata, void *main_data)
 	t_main	*m_data;
 
 	m_data = (t_main *)main_data;
-	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_RELEASE)
 		close_handler(m_data);
 }
