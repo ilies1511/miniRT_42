1128c3015837a1c072854b25d60e912c4e860fa5
Partttyyy <fa.rapp@web.de>
2024-09-12 20:24:36 +0200

    FIX(compile warnings):commendted some unused code to fix compile warning

--- srcs/ft_engine/utils/reset.c
+++ srcs/ft_engine/utils/reset.c
@@ -58,18 +58,18 @@ void	smoth_vanish(t_main *m_data)
 		while (x < WIDTH)
 		{
 			ist = pixels[y * WIDTH + x];
-			t_uint_color green = {.full = GREEN};
-			t_uint_color diff =
-			{
-				.argb.a = soll.argb.a - ist.argb.a,
-				.argb.r = soll.argb.r - ist.argb.r,
-				.argb.g = soll.argb.g - ist.argb.g,
-				.argb.b = soll.argb.b - ist.argb.b
-			};
-			int a_diff = soll.argb.a - ist.argb.a;
+			//t_uint_color green = {.full = GREEN};
+			//t_uint_color diff =
+			//{
+			//	.argb.a = soll.argb.a - ist.argb.a,
+			//	.argb.r = soll.argb.r - ist.argb.r,
+			//	.argb.g = soll.argb.g - ist.argb.g,
+			//	.argb.b = soll.argb.b - ist.argb.b
+			//};
+			//int a_diff = soll.argb.a - ist.argb.a;
 			int r_diff = soll.argb.r - ist.argb.r;
-			int g_diff = soll.argb.g - ist.argb.g;
-			int b_diff = soll.argb.b - ist.argb.b;
+			//int g_diff = soll.argb.g - ist.argb.g;
+			//int b_diff = soll.argb.b - ist.argb.b;
 			if (r_diff > 0)
 			{
 				pixels[y * WIDTH + x].argb.r++;
@@ -80,7 +80,7 @@ void	smoth_vanish(t_main *m_data)
 				pixels[y * WIDTH + x].argb.r--;
 				pixels[y * WIDTH + x].argb.g++;
 			}
-			float factor = m_data->mlx->delta_time / 20;
+			//float factor = m_data->mlx->delta_time / 20;
 			x++;
 		}
 		y++;
