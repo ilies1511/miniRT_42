29b5319b03d5aa7998ca28e02152e0c84d85644a
FabianRapp <fa.rapp@web.de>
2024-09-13 23:53:22 +0200

    CHORE(matrix tests): added 2 tests for mtx_inverse

--- srcs/ft_math/matrix/matrix_inverse.c
+++ srcs/ft_math/matrix/matrix_inverse.c
@@ -1,40 +1,65 @@
 #include <ft_matrix.h>
+#include <ft_floats.h>
+#include <libft.h>
 
 // inefficnet basic
 t_matrix	mtx_inverse(t_matrix m)
 {
 	t_matrix	inv;
-	t_matrix	cofac;
 	int			row;
 	int			col;
 	float		det;
 
-	cofac.type = m.type;
-	inv.type = m.type;
-	row = 0;
+	ft_assert(!eq_f(mtx_det(m), 0.0), __FILE__, __LINE__, "Error: matrix\
+			given to mtx_inverse() is not invertable (det == 0)");
 	det = mtx_det(m);
-	while (row < (int)m.type)
-	{
-		col = 0;
-		while (col < (int)m.type)
-		{
-			cofac.m[row][col] = mtx_cofactor(m, row, col);
-			col++;
-		}
-		row++;
-	}
-	cofac = mtx_transpose(cofac);
 	row = 0;
 	while (row < (int)m.type)
 	{
 		col = 0;
 		while (col < (int)m.type)
 		{
-			inv.m[row][col] = cofac.m[row][col] / det;
+			inv.m[col][row] = mtx_cofactor(m, row, col) / det;
 			col++;
 		}
 		row++;
 	}
-
+	inv.type = m.type;
 	return (inv);
 }
+//t_matrix	mtx_inverse(t_matrix m)
+//{
+//	t_matrix	inv;
+//	t_matrix	cofac;
+//	int			row;
+//	int			col;
+//	float		det;
+//
+//	cofac.type = m.type;
+//	inv.type = m.type;
+//	row = 0;
+//	det = mtx_det(m);
+//	while (row < (int)m.type)
+//	{
+//		col = 0;
+//		while (col < (int)m.type)
+//		{
+//			cofac.m[row][col] = mtx_cofactor(m, row, col);
+//			col++;
+//		}
+//		row++;
+//	}
+//	cofac = mtx_transpose(cofac);
+//	row = 0;
+//	while (row < (int)m.type)
+//	{
+//		col = 0;
+//		while (col < (int)m.type)
+//		{
+//			inv.m[row][col] = cofac.m[row][col] / det;
+//			col++;
+//		}
+//		row++;
+//	}
+//	return (inv);
+//}
--- srcs/ft_math/matrix/test_mtx_inverse.c
+++ srcs/ft_math/matrix/test_mtx_inverse.c
@@ -15,7 +15,7 @@ bool	test_mtx_inverse(void)
 		},
 		.type = MAT4X4,
 	};
-	t_matrix expected = {
+	t_matrix expected1 = {
 		.m = {
 			{0.21805, 0.45113, 0.24060, -0.04511},
 			{-0.80827, -1.45677, -0.44361, 0.52068},
@@ -25,13 +25,71 @@ bool	test_mtx_inverse(void)
 		.type = MAT4X4,
 	};
 	actual = mtx_inverse(test1);
-	if (!mtx_eq(actual, expected))
+	if (!mtx_eq(actual, expected1))
 	{
 		ret = false;
 		ft_fprintf(2, "Test failed: mtx_inverse: input:");
 		mtx_print(2, test1);
 		ft_fprintf(2, "expected: ");
-		mtx_print(2, expected);
+		mtx_print(2, expected1);
+		ft_fprintf(2, "actual: ");
+		mtx_print(2, actual);
+	}
+	t_matrix	test2 = {
+		.m = {
+			{8, -5, 9, 2},
+			{7, 5, 6, 1},
+			{-6, 0, 9, 6},
+			{-3, 0, -9, -4},
+		},
+		.type = MAT4X4,
+	};
+	t_matrix	expected2 = {
+		.m = {
+			{-0.15385, -0.15385, -0.28205, -0.53846},
+			{-0.07692, 0.12308, 0.02564, 0.03077},
+			{0.35897, 0.35897, 0.43590, 0.92308},
+			{-0.69231, -0.69231, -0.76923, -1.92308},
+		},
+		.type = MAT4X4,
+	};
+	actual = mtx_inverse(test2);
+	if (!mtx_eq(actual, expected2))
+	{
+		ret = false;
+		ft_fprintf(2, "Test failed: mtx_inverse: input:");
+		mtx_print(2, test2);
+		ft_fprintf(2, "expected: ");
+		mtx_print(2, expected2);
+		ft_fprintf(2, "actual: ");
+		mtx_print(2, actual);
+	}
+	t_matrix	test3 = {
+		.m = {
+			{9, 3, 0, 9},
+			{-5, -2, -6, -3},
+			{-4, 9, 6, 4},
+			{-7, 6, 6, 2},
+		},
+		.type = MAT4X4,
+	};
+	t_matrix	expected3 = {
+		.m = {
+			{-0.04074, -0.07778, 0.14444, -0.22222},
+			{-0.07778, 0.03333, 0.36667, -0.33333},
+			{-0.02901, -0.14630, -0.10926, 0.12963},
+			{0.17778, 0.06667, -0.26667, 0.33333},
+		},
+		.type = MAT4X4,
+	};
+	actual = mtx_inverse(test3);
+	if (!mtx_eq(actual, expected3))
+	{
+		ret = false;
+		ft_fprintf(2, "Test failed: mtx_inverse: input:");
+		mtx_print(2, test3);
+		ft_fprintf(2, "expected: ");
+		mtx_print(2, expected3);
 		ft_fprintf(2, "actual: ");
 		mtx_print(2, actual);
 	}
