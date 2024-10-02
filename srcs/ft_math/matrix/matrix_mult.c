6c1ffd5915b0e75224e544d69c20b5a7aa482dde
FabianRapp <fa.rapp@web.de>
2024-10-02 17:26:30 +0200

    NORM(matrix_mult.c)

--- srcs/ft_math/matrix/matrix_mult.c
+++ srcs/ft_math/matrix/matrix_mult.c
@@ -7,55 +7,26 @@ t_tuple	mtx_mult_mt(t_matrix m, t_tuple tup)
 {
 	t_tuple	res;
 	double	tmp[4];
+	int		i;
+	int		j;
 
 	ft_bzero(&res, sizeof(res));
 	ft_assert(m.type == MAT4X4, __FILE__, __LINE__,
 		"mult_mt: given matrix is not 4x4");
-	for (int i = 0; i < 4; i++)
+	i = 0;
+	while (i < 4)
 	{
-		for (int j = 0; j < 4; j++)
+		j = 0;
+		while (j < 4)
 		{
 			tmp[j] = m.m[i][j] * tup.arr[j];
+			j++;
 		}
 		res.arr[i] = kahan_sum(tmp, 4);
+		i++;
 	}
 	return (res);
 }
-
-//// assumes a 4x4 matrix
-//t_matrix	mtx_mult_mm(t_matrix ma, t_matrix mb)
-//{
-//	t_matrix	ret;
-//	int			i;
-//	int			j;
-//	int			k;
-//
-//	ft_assert(ma.type == MAT4X4 && mb.type == MAT4X4, __FILE__, __LINE__,
-//			"Error: matrix multiplication_mm of wrong matrix type");
-//	ft_bzero(&ret, sizeof ret);
-//	ret.type = MAT4X4;
-//	i = 0;
-//	while (i < 4)
-//	{
-//		j = 0;
-//		while (j < 4)
-//		{
-//			k = 0;
-//			while (k < 4)
-//			{
-//				ft_assert(
-//					fabs((double)ma.m[i][j] * (double)mb.m[j][k] + (double)ret.m[i][k])
-//					< FLT_MAX,
-//					__FILE__, __LINE__, "Error: float overflow mtx_mult_mm");
-//				ret.m[i][k] += ma.m[i][j] * mb.m[j][k];
-//				k++;
-//			}
-//			j++;
-//		}
-//		i++;
-//	}
-//	return (ret);
-//}
 
 t_matrix	mtx_mult_mm(t_matrix ma, t_matrix mb)
 {
@@ -66,12 +37,10 @@ t_matrix	mtx_mult_mm(t_matrix ma, t_matrix mb)
 	double		tmp[4];
 
 	ft_assert(ma.type == MAT4X4 && mb.type == MAT4X4, __FILE__, __LINE__,
-			"Error: matrix multiplication_mm of wrong matrix type");
-	ft_bzero(&ret, sizeof ret);
+		"Error: matrix multiplication_mm of wrong matrix type");
 	ret.type = MAT4X4;
-	i = 0;
-	//mb = mtx_transpose(mb);
-	while (i < 4)
+	i = -1;
+	while (++i < 4)
 	{
 		j = 0;
 		while (j < 4)
@@ -82,10 +51,8 @@ t_matrix	mtx_mult_mm(t_matrix ma, t_matrix mb)
 				tmp[k] = ma.m[i][k] * mb.m[k][j];
 				k++;
 			}
-			ret.m[i][j] = kahan_sum(tmp, 4);
-			j++;
+			ret.m[i][j++] = kahan_sum(tmp, 4);
 		}
-		i++;
 	}
 	return (ret);
 }
