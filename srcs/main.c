25cd734b6ce896ea6105c3e04ce3d9ace1539352
FabianRapp <fa.rapp@web.de>
2024-10-02 15:15:10 +0200

    NORM(main.c)

--- srcs/main.c
+++ srcs/main.c
@@ -6,10 +6,7 @@ int	main(int ac, char *av[])
 
 	m_data = get_m_data();
 	main_init(m_data, ac, av);
-	// this should never be reached sicne the mlx loop called in main_init()
-	// goes until main_cleanup() is called();
 	ft_assert(0, "end of main should never be reached", __LINE__, __FILE__);
 	__builtin_unreachable();
 	return (0);
 }
-
