c3dae3bc7240609c5743e306126d17afb116d972
ilies1511 <iziane@student.42heilbronn.de>
2024-10-06 21:53:47 +0200

    NORM(across files): small norm fixes

--- includes/ft_engine/ft_bump_map.h
+++ includes/ft_engine/ft_bump_map.h
@@ -7,8 +7,30 @@
 # include <ft_matrix.h>
 # include <ft_engine.h>
 
+typedef struct s_bump_norm
+{
+	double		wavelength[5];
+	double		amplitude[5];
+	double		directions[5][2];
+	double		phases[5];
+	double		cos_dot;
+	double		sin_dot;
+	int			i;
+	double		x;
+	double		z;
+	double		k;
+	double		dir_x;
+	double		dir_z;
+	double		d_dx;
+	double		d_dy;
+	double		d_dz;
+	t_vec		perturbed_normal;
+	double		dot;
+	double		dir_length;
+	const int	num_waves;
+}				t_bump_norm;
 
-			//Source Files
+//Source Files
 //Frame for Bump Map Logic
 t_bump		*bump_new_bump(void);
 t_vec		bump_normal_at(t_obj obj, t_bump bump_map, t_point world_point, t_vec normal_base);
@@ -16,18 +38,5 @@ t_vec		bump_normal_at(t_obj obj, t_bump bump_map, t_point world_point, t_vec nor
 //Bump map type (specific maps)
 t_bump		*bump_wave(void);
 t_vec		bump_wave_normal_at(t_bump bump, t_point point, t_vec base_normal);
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
--- includes/ft_engine/ft_engine.h
+++ includes/ft_engine/ft_engine.h
@@ -131,13 +131,13 @@ typedef enum e_obj_type
 const char		*eng_type_to_str(t_obj_type type, char buf[ERROR_BUF_LEN]);
 bool			eng_is_shape(t_obj_type type);
 
-typedef struct s_obj
+typedef struct	s_obj
 {
 	t_matrix	transform;
 	t_matrix	inverse;
 	t_obj_type	type;
 	t_material	material;
-}	__attribute__((may_alias)) t_obj;
+}	__attribute__((may_alias))	t_obj;
 
 typedef enum e_bump_type
 {
@@ -145,10 +145,10 @@ typedef enum e_bump_type
 	BUMP_WAVE,
 }	t_bump_type;
 
-typedef struct s_bump
+typedef struct	s_bump
 {
 	t_obj			base_obj;
-	t_bump_type	type;
+	t_bump_type		type;
 }	__attribute__((may_alias)) t_bump;
 
 typedef enum e_pattern_type
@@ -160,7 +160,7 @@ typedef enum e_pattern_type
 	PAT_SQUARE_3D
 }	t_pattern_type;
 
-typedef struct s_pattern
+typedef struct	s_pattern
 {
 	t_obj			base_obj;
 	t_pattern_type	type;
@@ -174,7 +174,7 @@ t_obj_ptr			eng_alloc_shape(t_obj_type type);
  * width/height in pixels
  a pixel_size: distance a signle pixel covers in 3d space
 */
-typedef struct s_camera
+typedef struct	s_camera
 {
 	t_obj	base_obj;
 	double	fov_x;
@@ -185,21 +185,21 @@ typedef struct s_camera
 	double	half_wall_height;
 }	__attribute__((may_alias)) t_camera;
 
-typedef struct s_sphere
+typedef struct	s_sphere
 {
 	t_obj		base_obj;
 	t_point		origin;
 	double		rad;
 }	__attribute__((may_alias))t_sphere;
 
-typedef struct s_plane
+typedef struct	s_plane
 {
 	t_obj		base_obj;
 }	__attribute__((may_alias))t_plane;
 
 t_plane			eng_new_plane(void);
 
-typedef struct s_cone
+typedef struct	s_cone
 {
 	t_obj		base_obj;
 	t_point		origin;
@@ -211,7 +211,7 @@ typedef struct s_cone
 
 t_cone		eng_new_cone(void);
 
-typedef struct s_cylinder
+typedef struct	s_cylinder
 {
 	t_obj		base_obj;
 	t_point		origin;
@@ -229,7 +229,7 @@ typedef struct s_intersc
 	t_obj_ptr	obj;
 }	t_intersc;
 
-typedef struct s_intersc_arr
+typedef struct	s_intersc_arr
 {
 	size_t		count;
 	t_intersc	*arr;
