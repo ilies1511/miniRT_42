b1ff2ffe947e4eb3d98243636b8267ff0dfb57de
ilies1511 <iziane@student.42heilbronn.de>
2024-10-06 18:25:44 +0200

    REFACTOR(normal_at): Add improved file structure for normal_at types (shapes)

--- srcs/ft_engine/objs/normal_at.c
+++ srcs/ft_engine/objs/normal_at.c
@@ -4,63 +4,6 @@
 #include <ft_floats.h>
 #include <ft_bump_map.h>
 
-/*
-	use case: unit circle sphere (sphere that was not tranformed yet)
-*/
-t_vec	compute_normal_sphere(t_point object_point)
-{
-	t_point		object_normal;
-
-	object_normal = sub_t(object_point, new_point(0, 0, 0));
-	return (object_normal);
-}
-
-t_vec	compute_normal_plane(t_point object_point)
-{
-	(void) object_point;
-	return (new_vec(0, 1, 0));
-}
-
-t_vec	compute_normal_cylinder(t_cylinder *cylinder, t_point object_point)
-{
-	double	distance;
-
-	ft_assert(cylinder->base_obj.type == OBJ_CYLINDER, __FILE__, __LINE__, "wtf");
-	distance = (object_point.x * object_point.x) + (object_point.z
-			* object_point.z);
-	if (distance < 1 && object_point.y >= cylinder->max - (EPSILON))
-		return (new_vec(0, 1, 0));
-	else if (distance < 1 && object_point.y <= cylinder->min + (EPSILON))
-		return (new_vec(0, -1, 0));
-	else
-		return (new_vec(object_point.x, 0, object_point.z));
-}
-
-t_vec	compute_normal_cone(t_cone *cone, t_point object_point)
-{
-	double	distance;
-	double	y;
-
-	//TODO: 2fix casea
-	if (eq_t(object_point, new_point(0, 0, 0)))
-		return (norm(new_vec(0, 1, 0)));
-	distance = (object_point.x * object_point.x) + \
-		(object_point.z * object_point.z);
-	//TODO: something here causes undefiend behaivior where depending of the compiler flags diffrent tests start failing
-	if (distance < 1 && object_point.y >= cone->max - (EPSILON))
-		return (new_vec(0, 1, 0));
-	else if (distance < 1 && object_point.y <= cone->min + (EPSILON))
-		return (new_vec(0, -1, 0));
-	else
-	{
-		y = sqrt((object_point.x * object_point.x) \
-			+ (object_point.z * object_point.z));
-		if (object_point.y > 0)
-			y = -y;
-		return (new_vec(object_point.x, y, object_point.z));
-	}
-}
-
 static t_vec	normal_at_iterate_types(t_obj_ptr object, t_point point_obj_space)
 {
 	if (object->type == OBJ_SPHERE)
@@ -90,6 +33,7 @@ t_vec	eng_normal_at(t_obj_ptr object, t_point intersec_point)
 		normal_obj_space);
 	normal_world_space.w = 0;
 	if (object->material.bump)
-		normal_world_space = bump_normal_at(*object, *object->material.bump, intersec_point, normal_world_space);
+		normal_world_space = bump_normal_at(*object, *object->material.bump, \
+			intersec_point, normal_world_space);
 	return (norm(normal_world_space));
 }
