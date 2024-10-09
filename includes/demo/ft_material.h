92ed019ddf835d87addfa907da85b68a4f16c745
ilies1511 <iziane@student.42heilbronn.de>
2024-10-09 22:38:41 +0200

    NORM(header): Add header

--- includes/demo/ft_material.h
+++ includes/demo/ft_material.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_material.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 22:32:48 by iziane            #+#    #+#             */
+/*   Updated: 2024/10/09 22:33:00 by iziane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_MATERIAL_H
-#define FT_MATERIAL_H
+# define FT_MATERIAL_H
 
 //Includes
-#include <ft_engine.h>
+# include <ft_engine.h>
 
 //Function Prototpyes
 //Metal
